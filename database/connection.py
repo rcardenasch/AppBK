5ba0c41c548cd572503c3e95d284503d1514b7e6
Romel Cardenas <romel.cardenas.chauca@gmail.com>
2026-09-24 22:05:24 -0500

    4to Commit

--- database/connection.py
+++ database/connection.py
@@ -4,11 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = (
-<<<<<<< HEAD
     "postgresql+psycopg2://postgres:1234@localhost:5432/BK_FAM"
-=======
-    "postgresql+psycopg2://postgres:123456@localhost:5433/BK_FAM"
->>>>>>> 9735a76f2390c160db2f78f98c7a1d0957fb9f46
 )
 
 engine = create_engine(
